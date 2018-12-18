6bd75b5cae958cc9587f53c81afcab5973acf6fd
Hogfeldt <hogfeldt38@gmail.com>
2018-12-18 13:12:46 +0100

    make_dict.py is looking for github in source. Added meta.yaml and jinja.txt for testing if we can convert meta.yaml to static files

--- make_dict.py
+++ make_dict.py
@@ -1,13 +1,58 @@
+from pathlib import Path
 import yaml
+import argparse
+import os
+
+
+numExc = 0
+
+def walklevel(some_dir, level=0):
+    some_dir = some_dir.rstrip(os.path.sep)
+    assert os.path.isdir(some_dir)
+    num_sep = some_dir.count(os.path.sep)
+    for root, dirs, files in os.walk(some_dir):
+        yield root, dirs, files
+        num_sep_this = root.count(os.path.sep)
+        if num_sep + level <= num_sep_this:
+            del dirs[:]
 
 def make_dict(path):
-    document = open(path, 'r').read()
-    return yaml.load(document)
+    try:
+        document = open(path, 'r').read()
+        return yaml.load(document)
+    except yaml.YAMLError as exc:
+        print(exc)
+        global numExc
+        numExc += 1
+
 
 if __name__ == "__main__":
-    meta_yaml_dict = make_dict("./meta.yaml")
-    print(yaml.dump(meta_yaml_dict))
+    parser = argparse.ArgumentParser(description="Runs through a bioconda recipes and operates on .yamel files")
+    parser.add_argument('bioconda_path', help='path to bioconda/recipes')
+    args = parser.parse_args()
 
-    print("#"*50)
-    print(meta_yaml_dict["package"])
-    print("#"*50)
+    numGithub = 0 
+    numDir = 0
+    dir_path = args.bioconda_path
+    for subdir, dirs, files in os.walk(dir_path):
+        for i in dirs:
+            print('Dirs: '+i)
+            path = "{}{}/meta.yaml".format(dir_path, i)
+            if not Path(path).is_file():
+                continue
+            print(path)
+            meta_yaml_dict = make_dict(path)
+            if meta_yaml_dict != None:
+                if 'source' in meta_yaml_dict:
+                    numDir += 1
+                    if isinstance(meta_yaml_dict['source'], list) and 'url' in meta_yaml_dict['source'][0]:
+                        url = meta_yaml_dict['source'][0]['url']
+                    elif 'url' in meta_yaml_dict['source']:
+                        url = meta_yaml_dict['source']['url']
+                    print(url)
+                    if 'github' in url:
+                        numGithub += 1
+
+            print('total: '+str(numGithub)+' of '+str(numDir))
+    print('total: '+str(numGithub)+' of '+str(numDir))
+    print('Number of Jinja exceptions: '+str(numExc))
