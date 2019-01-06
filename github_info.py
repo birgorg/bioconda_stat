53d5e81b197473a8ef10648d3ac2c51e85487d27
Hogfeldt <hogfeldt38@gmail.com>
2019-01-06 14:21:25 +0100

    github_info.py can now handle GithubRateExceededExceptions correctly

--- github_info.py
+++ github_info.py
@@ -5,29 +5,41 @@ import github
 import time
 import os
 
-def wait_for_rate_limit(user, repo, g):
-    while g.rate_limiting[0] < 100:
-        time.sleep(60)
-    getFiletreeForRepo(user, repo, g)
+def wait_for_rate_limit(g):
+    print(g.rate_limiting_resettime)
+    print(time.time())
+    while g.rate_limiting_resettime+3600 > time.time() :
+        print('Time limit exceeded, seconds remaining: %r' % (g.rate_limiting_resettime+3600-time.time()))
+        time.sleep(10)
+
 
 @functools.lru_cache()
-def getFiletreeForRepo(user, repo, g, f):
-    filetree = {}
-    try:
-        repo = g.get_repo('{}/{}'.format(user, repo))
-        contents = repo.get_contents('')
-
-        while len(contents) > 1:
-            file_content = contents.pop(0)
-            if file_content.type == 'dir':
-                contents.extend(repo.get_contents(file_content.path))
+def getFiletreeForRepo(user, repo, g):
+    while True:
+        try:
+            with open('github_info_results/%s-%s.txt'%(user, repo), 'w') as f:
+                f.write('#repo:%s/%s \n' % (user, repo))
+                print('Visiting:%s/%s \n' % (user, repo)) 
+                filetree = {}
+                repository = g.get_repo('{}/{}'.format(user, repo))
+                contents = repository.get_contents('')
+        
+                while len(contents) > 1:
+                    file_content = contents.pop(0)
+                    if file_content.type == 'dir':
+                        contents.extend(repository.get_contents(file_content.path))
+                    else:
+                        path = file_content.path
+                        print(path)
+                        f.write(path+'\n')
+                return
+        except github.GithubException as exc:
+            print(exc)
+            if isinstance(exc, github.RateLimitExceededException):
+                wait_for_rate_limit(g)
             else:
-                path = file_content.path
-                print(path)
-                f.write(path+'\n')
-    except github.GithubException as exc:
-        print(exc)
-        raise
+                raise
+
 
 def main():
     parser = argparse.ArgumentParser(description="Script for counting files in github repo")
@@ -49,19 +61,17 @@ def main():
         os.makedirs(result_path)
 
     visited_lines = 0
-    try:
-        for line in lines:
-            url = line.split('/')
-            user, repo = url[3:5]
-            with open('github_info_results/%s-%s.txt'%(user, repo), 'w') as f:
-                f.write('#repo:%s/%s \n' % (user, repo))
-                print('Visiting:%s/%s \n' % (user, repo)) 
-                getFiletreeForRepo(user, repo, g, f)
+    for line in lines:
+        url = line.split('/')
+        user, repo = url[3:5]
+        try:
+            getFiletreeForRepo(user, repo, g)
             visited_lines += 1
-    except github.GithubException as exc:
-        uf = open(args.file_path, 'w')
-        for line in lines[visited_lines:]:
-            uf.write(line)
+        except github.GithubException as exc:
+            uf = open(args.file_path, 'w')
+            for line in lines[visited_lines:]:
+                uf.write(line)
+
 
 if __name__=='__main__':
     main()
