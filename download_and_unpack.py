af80ede1458a039c20c0ba40175f041e25b296b1
Hogfeldt <hogfeldt38@gmail.com>
2019-01-18 14:35:40 +0100

    Some bugs have been fixed and the scipts is now running at the computer in the office

--- download_and_unpack.py
+++ download_and_unpack.py
@@ -8,9 +8,11 @@ import tarfile
 def download_package(url_to_package):
     if not url_to_package.startswith("http"):
         return ""
-    
-    r = requests.get(url_to_package, stream=True)
-    
+    try:
+        r = requests.get(url_to_package, stream=True)
+    except:
+        return ""
+
     if url_to_package.endswith(".tar.gz"):
         temp_file_path = "temp.tar.gz"
     elif url_to_package.endswith(".zip"):
@@ -25,19 +27,22 @@ def download_package(url_to_package):
     return temp_file_path
 
 def unpack_package(temp_file_path):
-
-    if temp_file_path == "temp.tar.gz":
-        tf = tarfile.open(temp_file_path)
-        path_to_dir = os.path.commonprefix(tf.getnames()).strip("/")
-        tf.extractall()
-        tf.close()
-    else:
-        zip_ref = zipfile.ZipFile(temp_file_path, 'r')
-        path_to_dir = zip_ref.namelist()[0].strip("/")
-        zip_ref.extractall()
-        zip_ref.close()
-    
-    return path_to_dir
+    try:
+        if temp_file_path == "temp.tar.gz":
+            tf = tarfile.open(temp_file_path)
+            #path_to_dir = os.path.commonprefix(tf.getnames()).replace('/','').replace('.','')
+            tf.extractall('tmp')
+            tf.close()
+        else:
+            zip_ref = zipfile.ZipFile(temp_file_path, 'r')
+            #path_to_dir = zip_ref.namelist()[0].replace('/','').replace('.','')
+            #print(zip_ref.filename)
+            zip_ref.extractall('tmp')
+            zip_ref.close()
+    except:
+        os.mkdir('tmp')
+        #os.mkdir('tmp/fail')
+        pass
 
 def build_filetree(startpath, name):
     with open('results/%s.txt' % name, 'w') as f:
@@ -48,8 +53,8 @@ def build_filetree(startpath, name):
                f.write(newroot + "/" + fp + "\n")
 
 
-def clean(temp_file_path, path_to_dir):
-    shutil.rmtree(path_to_dir)
+def clean(temp_file_path):
+    shutil.rmtree('tmp')
     os.remove(temp_file_path)
 
 if __name__ == "__main__":
@@ -58,8 +63,16 @@ if __name__ == "__main__":
             print(url)
             temp_file_path = download_package(url.strip("\n"))
             if temp_file_path != "":  
-                path_to_dir = unpack_package(temp_file_path)
-                build_filetree(path_to_dir, path_to_dir)
-                clean(temp_file_path, path_to_dir)
+                #path_to_dir = unpack_package(temp_file_path)
+                unpack_package(temp_file_path)
+                name = ''
+                path = 'tmp'
+                root, dirs, files = next(os.walk(path))
+                while len(dirs) == 1 and len(files) == 0:
+                    path += '/'+dirs[0]
+                    name = dirs[0]
+                    root, dirs, files = next(os.walk(path))
+                build_filetree(path, name)
+                clean(temp_file_path)
     
     
